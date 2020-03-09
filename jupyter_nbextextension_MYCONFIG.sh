6039bb10e4fcd6ed4774dc81a03d0077042f187b
Vismay Agrawal <vismayagrawal24@gmail.com>
2020-03-09 23:55:07 +0530

    Uploaded jupyter notebook nbextension config

---
+++ jupyter_nbextextension_MYCONFIG.sh
@@ -0,0 +1,16 @@
+jupyter nbextension enable autosavetime/main --sys-prefi
+jupyter nbextension enable codefolding/main --sys-prefix
+jupyter nbextension enable collapsible_headings/main --sys-prefix
+jupyter nbextension enable datestamper/main --sys-prefix
+jupyter nbextension enable execute_time/ExecuteTime --sys-prefix
+jupyter nbextension enable freeze/main --sys-prefix
+jupyter nbextension enable highlight_selected_word/main --sys-prefix
+jupyter nbextension enable hinterland/hinterland --sys-prefix
+jupyter nbextension enable printview/main --sys-prefix
+jupyter nbextension enable scratchpad/main --sys-prefix
+jupyter nbextension enable snippets_menu/main --sys-prefix
+jupyter nbextension enable splitcell/splitcell --sys-prefix
+jupyter nbextension enable toc2/main --sys-prefix
+jupyter nbextension enable toggle_all_line_numbers/main --sys-prefix
+jupyter nbextension enable varInspector/main --sys-prefix
+jupyter nbextension enable zenmode/main --sys-prefix
